5b4ed0d307df14350ecd4f05ed0082385b9aa9c7
camila <145385881+Camilaorg@users.noreply.github.com>
2023-12-12 09:05:11 -0600

    Add files via upload

---
+++ c_Biblioteca/src/c_biblioteca/Main.java
@@ -0,0 +1,23 @@
+package c_biblioteca;
+public class Main {
+    public static void main(String[] args){
+    Biblioteca biblioteca = new Biblioteca();
+        //Crear y agregar libros.
+    Libro libro1 = new Libro("El señor de los anillos", "J.R.R Tolkien", 1);
+    Libro libro2 = new Libro("Cien años de soledad", "Gabriel Garcia Marquez", 2);
+    biblioteca.agregarLibro(libro1);
+    biblioteca.agregarLibro(libro2);
+        //Crear y registrar usuarios.
+        Usuario user1 = new Usuario("Juan", 101);
+        Usuario user2 = new Usuario("Mario", 202);
+        biblioteca.registrarUsuario(user1);
+        biblioteca.registrarUsuario(user2);
+            //Prestar y devolver libros.
+            user1.prestarLibro(libro1);
+            user2.prestarLibro(libro2);
+            user1.devolverLibro(libro1);
+                //Mostrar información de la biblioteca.
+                biblioteca.mostrarCatalogo();
+                biblioteca.mostrarUsuario();
+    }
+}
